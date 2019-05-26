e8f3272e0b0b943c50e361fc6a8936a729059e40
satyajit baral <satyajit.baral.89@gmail.com>
2019-05-26 11:38:34 +0530

    get all discoverable with bonded status

--- app/src/main/java/com/example/bt/MainActivity.java
+++ app/src/main/java/com/example/bt/MainActivity.java
@@ -1,51 +1,84 @@
 package com.example.bt;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.view.View;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView lstvw;
-    private ArrayAdapter aAdapter;
-    private BluetoothAdapter bAdapter = BluetoothAdapter.getDefaultAdapter();
+    ListView listView;
+    private BluetoothAdapter mBluetoothAdapter;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
+    private Set<BluetoothDevice> pairedDevices;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button)findViewById(R.id.btnGet);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(bAdapter==null){
-                    Toast.makeText(getApplicationContext(),"Bluetooth Not Supported",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Set<BluetoothDevice> pairedDevices = bAdapter.getBondedDevices();
-                    ArrayList list = new ArrayList();
-                    if(pairedDevices.size()>0){
-                        for(BluetoothDevice device: pairedDevices){
-                            String devicename = device.getName();
-                            String macAddress = device.getAddress();
-                            list.add("Name: "+devicename+"MAC Address: "+macAddress);
-                        }
-                        lstvw = (ListView) findViewById(R.id.deviceList);
-                        aAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, list);
-                        lstvw.setAdapter(aAdapter);
-                    }
-                }
-            }
-        });
+        listView=(ListView)findViewById(R.id.listView);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter.startDiscovery();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
+
+        ///*
+
+
+
+        /*
+        ArrayList list = new ArrayList();
+        for(BluetoothDevice bt : pairedDevices) list.add(bt.getName());
+        Toast.makeText(getApplicationContext(), "Showing Paired Devices",Toast.LENGTH_SHORT).show();
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+
+        //*/
+
+
+    /*
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        for(BluetoothDevice device1: pairedDevices){
+            String devicename = device1.getName();
+            String macAddress = device1.getAddress();
+            mDeviceList.add("Name: "+devicename+"\n MAC Address: "+macAddress);
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, mDeviceList);
+        listView.setAdapter(adapter);
+    */
+
+
     }
 
 
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDeviceList.add(device.getName() + "\n" + device.getAddress() +"\n" + device.getBluetoothClass()+"\n"+device.getBondState());
+                Log.i("BT1", device.getName() + "\n" + device.getAddress());
+                listView.setAdapter(new ArrayAdapter<String>(context,android.R.layout.simple_list_item_1, mDeviceList));
+            }
+        }
+    };
 
 }
